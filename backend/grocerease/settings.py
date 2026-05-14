d44094c261f180d8e27ebbe980a261c504ff8ea3
jeanran <ranoajeanalie@gmail.com>
2026-05-14 18:14:40 +0800

    up

--- backend/grocerease/settings.py
+++ backend/grocerease/settings.py
@@ -198,10 +198,15 @@ CSRF_TRUSTED_ORIGINS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL  = os.getenv('EMAIL_HOST_USER')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'rjeanalie@gmail.com'
+EMAIL_HOST_PASSWORD = 'ceqovoxsuhoayppp'
+DEFAULT_FROM_EMAIL = 'rjeanalie@gmail.com'
+
+FRONTEND_URL = 'http://127.0.0.1:8000'
