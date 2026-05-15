0b467e07588934bafddb9739ccad32101613d0c5
Jin <masterdepongasi@gmail.com>
2026-05-15 18:34:06 +0800

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -6,6 +7,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import POSScreen from './screens/POSScreen';
 import AdminDashboard from './screens/AdminDashboard';
+import StockInScreen from './screens/StockInScreen';
+import StockInHistoryScreen from './screens/StockInHistoryScreen';
+import StockOutScreen from './screens/StockOutScreen';
+import InventoryScreen from './screens/InventoryScreen';
+import UsersManagementScreen from './screens/UsersManagementScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +25,11 @@ export default function App() {
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="POSScreen" component={POSScreen} />
                 <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
+                <Stack.Screen name="StockIn" component={StockInScreen} />
+                <Stack.Screen name="StockInHistory" component={StockInHistoryScreen} />
+                <Stack.Screen name="StockOut" component={StockOutScreen} />
+                <Stack.Screen name="Inventory" component={InventoryScreen} />
+                <Stack.Screen name="Users" component={UsersManagementScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
