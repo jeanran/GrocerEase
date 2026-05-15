c8437141694cc05dfe4802c966f5653930eb2ef4
ssalvaleon <salvaleon.stephanie15@gmail.com>
2026-05-15 11:46:49 +0800

    Updated

--- backend/core/views.py
+++ backend/core/views.py
@@ -88,6 +88,23 @@ def dashboard(request):
     })
 
 
+def stock_in(request):
+    if not is_logged_in(request):
+        return redirect('login')
+    return render(request, 'stock_in.html', {
+        'username': request.session.get('username'),
+        'role':     request.session.get('role'),
+    })
+
+def stock_in_history(request):
+    """Page view for stock in transaction history"""
+    if not is_logged_in(request):
+        return redirect('login')
+    return render(request, 'stock_in_history.html', {
+        'username': request.session.get('username'),
+        'role':     request.session.get('role'),
+    })
+
 
 
 
